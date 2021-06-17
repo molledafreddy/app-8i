eff9aa6a7aad2261568835f4d74c1020566e5ab0
Freddy MOlleda <molledafreddy@gmail.com>
2021-06-16 22:09:00 -0400

    se realizaron modelos, factories y migraciones con su estructura de datos

---
+++ database/migrations/2021_06_17_020054_create_shipping_addresses_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShippingAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shipping_addresses', function (Blueprint $table) {
+            $table->id();
+            $table->string('direction')->lenght(200);
+            $table->string('department')->lenght(30);
+            $table->integer('number');
+            $table->string('commune')->lenght(200);
+            $table->string('region')->lenght(200);
+            $table->unsignedBigInteger('order_id');
+            $table->foreign('order_id')->references('id')->on('orders');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shipping_addresses');
+    }
+}
